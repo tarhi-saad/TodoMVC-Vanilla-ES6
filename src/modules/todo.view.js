08925dd541f9682ae07757e9d67fbf3d17ffe1d5
Saad TARHI <saad.trh@gmail.com>
2020-01-26 13:35:12 +0100

    Fix todoCount bug in default projects

--- src/modules/todo.view.js
+++ src/modules/todo.view.js
@@ -83,10 +83,11 @@ const todoView = () => {
     const todoCount = createElement('span', '.todo-count');
     let count = 0;
 
-    // Get todo count for default project on init()
+    // Get todo count elements for default project on init()
     let myDayCount = null;
     let ImportantCount = null;
     let PlannedCount = null;
+
     if (id > 4) {
       myDayCount = getElement('.list[data-index="2"] .todo-count');
       ImportantCount = getElement('.list[data-index="3"] .todo-count');
@@ -94,26 +95,30 @@ const todoView = () => {
     }
 
     items.forEach((todo) => {
-      if (!todo.isComplete) count += 1;
+      if (todo.isComplete) return;
 
-      if (todo.isMyDay) {
-        myDayCount.textContent = Number(myDayCount.textContent) + 1;
-      }
-      if (todo.isImportant) {
-        ImportantCount.textContent = Number(ImportantCount.textContent) + 1;
-      }
-      if (todo.date) {
-        PlannedCount.textContent = Number(PlannedCount.textContent) + 1;
-      }
+      count += 1;
 
-      if (id > 4) {
-        if (myDayCount.textContent === '1') showElement(myDayCount);
+      if (todo.isMyDay) myDayCount.textContent = Number(myDayCount.textContent) + 1;
 
-        if (ImportantCount.textContent === '1') showElement(ImportantCount);
+      if (todo.isImportant) ImportantCount.textContent = Number(ImportantCount.textContent) + 1;
 
-        if (PlannedCount.textContent === '1') showElement(PlannedCount);
-      }
+      if (todo.date) PlannedCount.textContent = Number(PlannedCount.textContent) + 1;
     });
+
+    if (id > 4) {
+      if (myDayCount.classList.contains('hide') && Number(myDayCount.textContent) > 0) {
+        showElement(myDayCount);
+      }
+
+      if (ImportantCount.classList.contains('hide') && Number(ImportantCount.textContent) > 0) {
+        showElement(ImportantCount);
+      }
+
+      if (PlannedCount.classList.contains('hide') && Number(PlannedCount.textContent) > 0) {
+        showElement(PlannedCount);
+      }
+    }
 
     todoCount.textContent = count;
 
